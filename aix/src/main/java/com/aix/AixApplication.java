2c672277ff95370a50e668fe7cc9c785cd4c254c
nmg20 <nicogm977@gmail.com>
2025-08-29 11:54:03 +0200

    initial commit: base

---
+++ aix/src/main/java/com/aix/AixApplication.java
@@ -0,0 +1,13 @@
+package com.aix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AixApplication.class, args);
+	}
+
+}
---
+++ aix/src/test/java/com/aix/AixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
